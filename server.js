8713986a07befdeecc4277bfb64c2e137ed3076c
Hunny Jaglen <104228915+HunnyJ434@users.noreply.github.com>
2025-05-03 15:28:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 const { Server } = require('socket.io');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = false;
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
